1d6b5c007837e116e970394a450290e1478a1bce
leafarmer <leafdevelop@gmail.com>
2016-11-18 08:16:08 +0000

    Added in missing initializer for generating war artifact

---
+++ src/main/java/uk/co/leafdevelopment/ServletInitializer.java
@@ -0,0 +1,13 @@
+package uk.co.leafdevelopment;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CdCsdApplication.class);
+	}
+
+}
